3f11d54f5faa2ae259ce4a85cc33a1d4d0aa5a35
Rowen Zaal <rowen.zaal@hva.nl>
2025-03-18 17:22:34 +0100

    Added screens.

---
+++ app/src/main/java/com/example/level5task2/ui/screens/MoviesScreens.kt
@@ -0,0 +1,7 @@
+package com.example.level5task2.ui.screens
+
+sealed class MoviesScreens(
+    val route: String
+) {
+
+}
---
+++ app/src/main/java/com/example/level5task2/ui/screens/MoviesSearchScreen.kt
@@ -0,0 +1,6 @@
+package com.example.level5task2.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MoviesSearchScreen() {}
