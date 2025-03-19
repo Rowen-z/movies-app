a40c8be46e48174f170b645bb107228ebbf60d3f
Rowen Zaal <rowen.zaal@hva.nl>
2025-03-19 16:43:06 +0100

    Implemented viewmodel & repository to retrieve movies with a query.

---
+++ app/src/main/java/com/example/level5task2/repository/MoviesRepository.kt
@@ -0,0 +1,25 @@
+package com.example.level5task2.repository
+
+import android.util.Log
+import com.example.level5task2.data.api.MoviesApi
+import com.example.level5task2.data.api.MoviesApiService
+import com.example.level5task2.data.api.util.Resource
+import com.example.level5task2.data.model.MoviesResponse
+import kotlinx.coroutines.withTimeout
+
+class MoviesRepository {
+    private val _moviesApiService: MoviesApiService = MoviesApi.createApi()
+
+    suspend fun getMovies(query: String): Resource<MoviesResponse> {
+        val response = try {
+            withTimeout(5_000) {
+                _moviesApiService.getMovies(query)
+            }
+        } catch (e: Exception) {
+            Log.e("MoviesRepository", e.message ?: "No exception message available")
+            return Resource.Error("An unknown error occurred while fetching data from the movies api.")
+        }
+
+        return Resource.Success(response)
+    }
+}
---
+++ app/src/main/java/com/example/level5task2/viewmodel/MoviesViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.level5task2.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.level5task2.data.api.util.Resource
+import com.example.level5task2.data.model.MoviesResponse
+import com.example.level5task2.repository.MoviesRepository
+import kotlinx.coroutines.launch
+
+class MoviesViewModel(application: Application): AndroidViewModel(application) {
+    private val _moviesRepository = MoviesRepository()
+
+    private val _moviesResource: MutableLiveData<Resource<MoviesResponse>> = MutableLiveData(Resource.Empty())
+
+    val moviesResource: LiveData<Resource<MoviesResponse>>
+        get() = _moviesResource
+
+    fun getMovies(query: String) {
+        _moviesResource.value = Resource.Loading()
+
+        viewModelScope.launch {
+            _moviesResource.value = _moviesRepository.getMovies(query)
+        }
+    }
+}
